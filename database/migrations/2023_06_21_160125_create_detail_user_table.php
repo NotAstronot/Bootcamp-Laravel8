096194c75c06c45125c27153b8fa7dfc612e946d
NotAstronot <feririski409@gmail.com>
2023-06-23 11:29:38 +0700

    foreignid first

--- database/migrations/2023_06_21_160125_create_detail_user_table.php
+++ database/migrations/2023_06_21_160125_create_detail_user_table.php
@@ -15,8 +15,8 @@ class CreateDetailUserTable extends Migration
     {
         Schema::create('detail_user', function (Blueprint $table) {
             $table->id();
-            $table->integer('user_id');
-            $table->integer('type_user_id');
+            $table->foreignId('user_id')->nullabe()->index('fk_detail_user_to_users');
+            $table->foreignId('type_user_id')->nullabe()->index('fk_detail_user_to_type_user');
             $table->string('contact')->nullabe();
             $table->longText('address')->nullabe();
             $table->longText('photo')->nullabe();
