8b8e7d285036877824b642d87d4b0474609111ee
NotAstronot <feririski409@gmail.com>
2023-06-23 15:28:51 +0700

    migrate keys

--- database/migrations/2023_06_23_035634_add_foreign_keys_to_detail_user_table.php
+++ database/migrations/2023_06_23_035634_add_foreign_keys_to_detail_user_table.php
@@ -14,12 +14,10 @@ class AddForeignKeysToDetailUserTable extends Migration
     public function up()
     {
         Schema::table('detail_user', function (Blueprint $table) {
-            $table->foreign('user_id', 'fk_detail_user_to_users')
-            ->reference('id')->on('user->')->onDelete('cascade')
-            ->onUpdate('cascade');
-            $table->foreign('type_user_id', 'fk_detail_user_to_type_user')
-            ->reference('id')->on('user->')->onDelete('cascade')
-            ->onUpdate('cascade');
+            $table->foreign('user_id', 'fk_detail_user_to_users')->references('id')->on
+            ('users')->onUpdate('CASCADE')->onDelete('CASCADE');
+            $table->foreign('type_user_id', 'fk_detail_user_to_type_user')->references('id')
+            ->on('type_user')->onUpdate('CASCADE')->onDelete('CASCADE');
         });
     }
 
