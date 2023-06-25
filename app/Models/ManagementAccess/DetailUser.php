25852a389227a3a62aa35b92e19533ab03d30689
NotAstronot <feririski409@gmail.com>
2023-06-26 01:34:34 +0700

    model sesi pertama

---
+++ app/Models/ManagementAccess/DetailUser.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models\ManagementAccess;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class DetailUser extends Model
+{
+    //use HasFactory;
+
+    use SoftDeletes;
+
+    public $table = 'detail_user';
+
+    protected $date = [
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
+
+    protected $fillable = [
+        'user_id',
+        'type_user_id',
+        'contact',
+        'address',
+        'photo',
+        'gender',
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
+};
