56be008333e29eb0aec8fd910991ed151626bb56
NotAstronot <feririski409@gmail.com>
2023-06-25 12:37:33 +0700

    Keys migration dan Masterdata

--- database/migrations/2023_06_23_064223_add_foreign_keys_to_permission_role_table.php
+++ database/migrations/2023_06_23_064223_add_foreign_keys_to_permission_role_table.php
@@ -14,12 +14,8 @@ class AddForeignKeysToPermissionRoleTable extends Migration
     public function up()
     {
         Schema::table('permission_role', function (Blueprint $table) {
-            $table->foreign('permission_id', 'fk_permission_role_to_permission')
-            ->references('id')->on('permission')->onDelete('cascade')
-            ->onUpdate('cascade');
-            $table->foreign('role_id', 'fk_permission_role_to_role')
-            ->references('id')->on('role')->onDelete('cascade')
-            ->onUpdate('cascade');
+            $table->foreign('permission_id', 'fk_permission_role_to_permission')->references('id')->on('permission')->onUpdate('CASCADE')->onDelete('CASCADE');
+            $table->foreign('role_id', 'fk_permission_role_to_role')->references('id')->on('role')->onUpdate('CASCADE')->onDelete('CASCADE');
         });
     }
 
